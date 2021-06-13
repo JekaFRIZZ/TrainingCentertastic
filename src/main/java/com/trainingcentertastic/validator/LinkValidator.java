88445f9a57c1ca99ea3eaa1ec031eda2973e6e5c
Женя <lapko.eugene@gmail.com>
2021-06-13 10:14:06 +0300

    create validator

---
+++ src/main/java/com/trainingcentertastic/validator/LinkValidator.java
@@ -0,0 +1,11 @@
+package com.trainingcentertastic.validator;
+
+public class LinkValidator {
+
+    private String REGEX = "\\b(https?|http)://github.com/[-a-zA-Z0-9+&#/%?=~_|!:,.;]*[-a-zA-Z0-9+&#/%=~_|]";
+
+    public boolean isLink(String text) {
+        return text.matches(REGEX);
+    }
+
+}
