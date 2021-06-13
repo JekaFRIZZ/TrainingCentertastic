8a70928f19ef04ec5fb600bab769a76fc19733df
Женя <lapko.eugene@gmail.com>
2021-06-13 10:11:45 +0300

    update entity

--- src/main/java/com/trainingcentertastic/entity/Identifiable.java
+++ src/main/java/com/trainingcentertastic/entity/Identifiable.java
@@ -1,5 +1,10 @@
 package com.trainingcentertastic.entity;
 
+/**
+ *  Each class is marked, an entity that has a field id
+ *
+ * @author Eugene Lapko
+ */
 public interface Identifiable {
 
     Long getId();
